e526ecfefd455a7f01718c204c36e09a860c1335
hskang9 <hskang9@gmail.com>
2020-01-14 14:22:38 -0800

    feat: add trait object clone and statement description

--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -1,23 +1,40 @@
 use crate::token::*;
+use std::clone::Clone;
 
+//TODO: Leave it until Rust supports multiple inheritance for Box<{any struct with interface}>
+/*
 pub trait Node {
     fn token_literal(&self) -> String;
 }
+*/
 
 pub trait Statement {
     fn token_literal(&self) -> String;
     fn statement_node(&self);
+    fn string(&self) -> String;
+    fn box_clone(&self) -> Box<dyn Statement>;
+}
+
+impl Clone for Box<dyn Statement> {
+    fn clone(&self) -> Box<dyn Statement> {
+        self.box_clone()
+    }
 }
 
 pub trait Expression {
     fn token_literal(&self) -> String;
     fn expression_node(&self);
+    fn string(&self) -> String;
+    fn box_clone(&self) -> Box<dyn Expression>;   
 }
 
-pub trait Draw {
-    fn draw(&self);
+impl Clone for Box<dyn Expression> {
+    fn clone(&self) -> Box<dyn Expression> {
+        self.box_clone()
+    }
 }
 
+#[derive(Clone)]
 pub struct Program {
     pub statements: Vec<Box<dyn Statement>>,
 }
@@ -31,9 +48,21 @@ impl Statement for Program {
         }
     }
     fn statement_node(&self) {}
+    fn string(&self) -> String {
+        let mut out = "";
+        for s in self.statements {
+            [out, &s.token_literal()].concat();
+        }
+        return out.to_string();
+    }
+    fn box_clone(&self) -> Box<dyn Statement> {
+        Box::new((*self).clone())
+    }
 }
 
+
 // Let Statment
+#[derive(Clone)]
 pub struct LetStatement {
     pub token: Token,
     pub name: Option<Identifier>,
@@ -45,9 +74,21 @@ impl Statement for LetStatement {
         return self.token.literal.to_string();
     }
     fn statement_node(&self) {}
+    fn string(&self) -> String {
+        let out = &[self.token_literal(), " ".to_string(), self.name.clone().unwrap().value, " = ".to_string()].concat();
+
+        if self.value.is_some() {
+            let out2 = [out.clone(), self.value.clone().unwrap().string()].concat();
+            out2.to_string();
+        }
+        return out.to_string();
+    }
+    fn box_clone(&self) -> Box<dyn Statement> {
+        Box::new((*self).clone())
+    }
 }
 
-#[derive(Clone, PartialEq)]
+#[derive(Clone)]
 pub struct Identifier {
     pub token: Option<Token>,
     pub value: String,
@@ -58,9 +99,16 @@ impl Expression for Identifier {
         return self.token.clone().unwrap().literal;
     }
     fn expression_node(&self) {}
+    fn string(&self) -> String {
+        return self.value.clone();
+    }
+    fn box_clone(&self) -> Box<dyn Expression> {
+        Box::new((*self).clone())
+    }
 }
 
 // Return statement
+#[derive(Clone)]
 pub struct ReturnStatement {
     pub token: Token,
     pub return_value: Option<Box<dyn Expression>>,
@@ -71,12 +119,26 @@ impl Statement for ReturnStatement {
         return self.token.literal.to_string();
     }
     fn statement_node(&self) {}
+    fn string(&self) -> String {
+        let out = &[&self.token_literal(), " "].concat();
+        
+        if self.return_value.is_some() {
+            let out2 = &[out.clone(), self.return_value.clone().unwrap().string()].concat();
+            return out2.to_string();
+        }
+        
+        return out.to_string();
+    }
+    fn box_clone(&self) -> Box<dyn Statement> {
+        Box::new((*self).clone())
+    }
 }
 
 // Expression statement
+#[derive(Clone)]
 pub struct ExpressionStatement {
     pub token: Token,
-    pub Expression: Option<Box<dyn Expression>>,
+    pub expression: Option<Box<dyn Expression>>,
 }
 
 impl Statement for ExpressionStatement {
@@ -84,4 +146,13 @@ impl Statement for ExpressionStatement {
         return self.token.literal.to_string();
     }
     fn statement_node(&self) {}
+    fn string(&self) -> String  {
+        if self.expression.is_some() {
+            return self.expression.clone().unwrap().string();
+        }
+        return "".to_string();
+    }
+    fn box_clone(&self) -> Box<dyn Statement> {
+        Box::new((*self).clone())
+    }
 }
